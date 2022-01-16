9fb3567d94ae4ff6ca4826d22ce0fee23e923058
takuya-nakayasu <l08084.1989@gmail.com>
2022-01-16 17:46:36 +0900

    add button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,44 @@ export class HomePage {
 
   constructor() {}
 
+  public shareWithOptionsFile(): void {
+    console.log('shareWithOptionsFile');
+  }
+
+  public shareWithOptionsFileArray(): void {
+    console.log('shareWithOptionsFileArray');
+  }
+
+  public shareViaEmailFile(): void {
+    console.log('shareViaEmailFile');
+  }
+
+  public shareViaEmailFileArray(): void {
+    console.log('shareViaEmailFileArray');
+  }
+
+  public shareFile(): void {
+    console.log('shareFile');
+  }
+
+  public shareFileArray(): void {
+    console.log('shareFileArray');
+  }
+
+  public saveToPhotoAlbumFile(): void {
+    console.log('saveToPhotoAlbumFile');
+  }
+
+  public saveToPhotoAlbumFileArray(): void {
+    console.log('saveToPhotoAlbumFileArray');
+  }
+
+  public shareViaWhatsAppToPhoneFile(): void {
+    console.log('shareViaWhatsAppToPhoneFile');
+  }
+
+  public shareViaWhatsAppToPhoneFileArray(): void {
+    console.log('shareViaWhatsAppToPhoneFileArray');
+  }
+
 }
