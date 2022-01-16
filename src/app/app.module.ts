7874c294ef0f85738de80bca6e661430a1c160d4
takuya-nakayasu <l08084.1989@gmail.com>
2022-01-17 00:47:41 +0900

    call social sharing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SocialSharing,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,47 +7,84 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public message = 'share this';
+  public subject = 'the subject';
+  public fileUrl = `data:image/png;base64,iVBORw0KGgoAAA
+  ANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4
+  //8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU
+  5ErkJggg==`;
+  public to = 'l08084.1989@gmail.com';
+  public phoneNumber = '09071879734';
 
-  constructor() {}
+  constructor(private socialSharing: SocialSharing) {}
 
   public shareWithOptionsFile(): void {
     console.log('shareWithOptionsFile');
+    this.socialSharing.shareWithOptions({ files: this.fileUrl });
   }
 
   public shareWithOptionsFileArray(): void {
     console.log('shareWithOptionsFileArray');
+    this.socialSharing.shareWithOptions({ files: [this.fileUrl] });
   }
 
   public shareViaEmailFile(): void {
     console.log('shareViaEmailFile');
+    this.socialSharing.shareViaEmail(
+      this.message,
+      this.subject,
+      [this.to],
+      undefined,
+      undefined,
+      this.fileUrl
+    );
   }
 
   public shareViaEmailFileArray(): void {
     console.log('shareViaEmailFileArray');
+    this.socialSharing.shareViaEmail(
+      this.message,
+      this.subject,
+      [this.to],
+      undefined,
+      undefined,
+      [this.fileUrl]
+    );
   }
 
   public shareFile(): void {
     console.log('shareFile');
+    this.socialSharing.share(undefined, undefined, this.fileUrl);
   }
 
   public shareFileArray(): void {
     console.log('shareFileArray');
+    this.socialSharing.share(undefined, undefined, [this.fileUrl]);
   }
 
   public saveToPhotoAlbumFile(): void {
     console.log('saveToPhotoAlbumFile');
+    this.socialSharing.saveToPhotoAlbum(this.fileUrl);
   }
 
   public saveToPhotoAlbumFileArray(): void {
     console.log('saveToPhotoAlbumFileArray');
+    this.socialSharing.saveToPhotoAlbum([this.fileUrl]);
   }
 
   public shareViaWhatsAppToPhoneFile(): void {
     console.log('shareViaWhatsAppToPhoneFile');
+    this.socialSharing.shareViaWhatsAppToPhone(
+      this.phoneNumber,
+      this.message,
+      this.fileUrl
+    );
   }
 
   public shareViaWhatsAppToPhoneFileArray(): void {
     console.log('shareViaWhatsAppToPhoneFileArray');
+    this.socialSharing.shareViaWhatsAppToPhone(this.phoneNumber, this.message, [
+      this.fileUrl,
+    ]);
   }
-
 }
